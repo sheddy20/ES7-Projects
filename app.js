4115c986479e6a33432e461981cf17cf9b709a65
Abel Shedrack Nicholas <shedrackejeh458@gmail.com>
2020-08-23 19:59:38 +0100

    change

---
+++ app.js
@@ -0,0 +1,12 @@
+//let datatypes:
+function welcomeMe(){
+    for (let i = 0; i < 5; i++){
+        console.log(i);
+    }
+}
+
+welcomeMe();
+//const datatypes:
+const pi = 3.142;
+pi = 'hello world';
+console.log(pi);
