b0b2effeac9943f3538d6be1be63628658d4b002
Abel Shedrack Nicholas <shedrackejeh458@gmail.com>
2020-08-28 23:25:59 +0100

    changes

--- array.map.js
+++ array.map.js
@@ -1,4 +1,19 @@
-const groceries = ['cumcumber', 'vegetables', 'lettuce', 'tomato'];
+// const groceries = ['cumcumber', 'vegetables', 'lettuce', 'tomato'];
 
-const mystore = groceries.map(store => `<li> ${store} </li>`);
-console.log(mystore);
+// const mystore = groceries.map(store => `<li> ${store} </li>`);
+// console.log(mystore);
+
+let pi = new Promise((resolve, reject)=>{
+    const valueOfPi = 3.143;
+    if (valueOfPi == 3.142) {
+        resolve('value of pi is correct');
+    } else {
+        reject('value of pi is incorrect');
+    }
+})
+
+pi.then((m)=>{
+    console.log(`${m}`);
+}).catch((e)=>{
+    console.log(`${e}`);
+});
--- destructuring.js
+++ destructuring.js
@@ -1,7 +1,17 @@
-const address = {
-    street: 'Lincoln yusuf street agwan jikowyi',
-    city: 'New Karu',
-    country: 'Nigeria',
+// const address = {
+//     street: 'Lincoln yusuf street agwan jikowyi',
+//     city: 'New Karu',
+//     country: 'Nigeria',
+// }
+
+// const { street, city, country} = address;
+
+function sayHi(name, age){
+    console.log(`hello ${name} welcome to vue crash course`);
+    console.log(`your are ${age} years old`);
 }
 
-const { street, city, country} = address;
+sayHi('antony', 45);
+sayHi('loveth', 35);
+sayHi('shedrack', 22);
+sayHi('mason', 19);
