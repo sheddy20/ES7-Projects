05a8d38f53713daa2d210dc5a2b4f8d6f3bc8732
Abel Shedrack Nicholas <shedrackejeh458@gmail.com>
2020-08-28 23:27:09 +0100

    changes

--- spread.js
+++ spread.js
@@ -1,11 +1,11 @@
-// function mynumber(n){
-//     let number = 0;
-//     for(let i = 0; i <= n; i++){
-//         number += i;
-//     }
-//     return number;
-// }
+function mynumber(n){
+    let number = 0;
+    for(let i = 0; i <= n; i++){
+        number += i;
+    }
+    return number;
+}
 
-// let result = mynumber(3);
-// console.log(result);
+let result = mynumber(3);
+console.log(result);
 
